fd375f50b04842565bc630e5ab48bbd189151214
jijung1 <jijung@chapman.edu>
2019-02-06 14:52:11 +0000

    update to helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
+/*
+Name:                   Jin Jung
+Student ID:             2329401
+Email:                  jijung@chapman.edu
+Course no. & Section:   CS350-02
+Assignment:             HelloWorld
+*/
 
+/*
+  Test program to verify capability to use github to submit assignments. Program outputs "Hello World\n" to terminal.
+*/
+
+//pre-processor directive
+#include <iostream>
+//using std namespace
 using namespace std;
 
 int main()
 {
   cout << "Hello World\n";
+  //pause until user presses return 
   getchar();
   return 0;
 }
